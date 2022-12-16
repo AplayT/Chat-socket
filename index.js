878778ea0a0a3c0a013ffd239fa4c0cce4698dcd
AT <jpacedo1@gmail.com>
2022-12-16 16:52:15 -0500

    Comunicacion exitosa entre usuarios

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+app.use(express.static(__dirname + '/templates'));
+
+
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+
+app.get('/', (req,res) => {
+   res.sendFile(__dirname + '/templates/index.html')
+});
+
+io.on('connection', (socket) => {
+   console.log('Un usuario se conecto '+ socket.id)
+
+   socket.on('chat message', (msg) => {
+        console.log('nsg '+msg);
+        io.emit('chat message', msg);
+   });
+
+   socket.on('disconnect', () => {
+      console.log('se desconecto')
+   });
+});
+
+server.listen(8080, () => {
+   console.log('Escuchando 8080')
+});
+
